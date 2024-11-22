42dcde36138492e5d7a61131f90c29fcdecf3fb1
Hannah Stanton-Gockel <156378676+gohannahux@users.noreply.github.com>
2024-11-21 20:47:03 -0500

    Add files via upload

--- video.js
+++ video.js
@@ -8,17 +8,18 @@ window.addEventListener("load", function() {
 // document.querySelector("#play").addEventListener("click", function() {
 // 	console.log("Play Video");
 // });
-
 document.querySelector("#play").addEventListener("click", function() {
 	video.play();
 	console.log("Play Video");
 }
 
+/*pause video*/
 document.querySelector("#pause").addEventListener("click", function() {
 	video.pause();
 	console.log("Pause Video");
-}	
+}
 
+/*slow down video*/
 document.querySelector("#slower").addEventListener("click", function() {
 	video.playbackRate *= 0.9;
 	console.log("Speed is " + video.playbackRate);
@@ -31,8 +32,37 @@ document.querySelector("#faster").addEventListener("click", function() {
 }
 
 /*skip ahead*/
-document.querySelector("#skip").addEventListener("click", function() {
+.document.querySelector("#skip").addEventListener("click", function() {
 	if (video.currentTime += 10 > video.duration) {
 		video.currentTime = 0;
 		console.log("Video Current Time is " + video.currentTime);
 	}
+
+
+/*mute video*/
+.document.querySelector("#mute").addEventListener("click", function() {
+	if (video.muted) {
+		video.muted = false;
+		console.log("Unmuted");
+	} else {
+		video.muted = true;
+		console.log("Muted");
+	}
+}
+
+/*volume slider*/
+.document.querySelector("#slider").addEventListener("click", function() {
+	video.volume = this.value / 100;
+	console.log("Volume is " + video.volume);
+}
+
+
+/*old school*/
+.document.querySelector("#vintage").addEventListener("click", function() {
+	video.classList.add("oldSchool");
+}
+
+/*original*/
+.document.querySelector("#orig").addEventListener("click", function() {
+	video.classList.remove("oldSchool");
+}
