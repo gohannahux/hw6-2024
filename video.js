1ec4a1db5c138eec254c5ff0e33f72e5975b0da3
Hannah Stanton-Gockel <156378676+gohannahux@users.noreply.github.com>
2024-11-21 20:34:47 -0500

    Add files via upload

--- video.js
+++ video.js
@@ -9,3 +9,30 @@ window.addEventListener("load", function() {
 // 	console.log("Play Video");
 // });
 
+document.querySelector("#play").addEventListener("click", function() {
+	video.play();
+	console.log("Play Video");
+}
+
+document.querySelector("#pause").addEventListener("click", function() {
+	video.pause();
+	console.log("Pause Video");
+}	
+
+document.querySelector("#slower").addEventListener("click", function() {
+	video.playbackRate *= 0.9;
+	console.log("Speed is " + video.playbackRate);
+}
+
+/*speed up video*/
+document.querySelector("#faster").addEventListener("click", function() {
+	video.playbackRate /= 0.9;
+	console.log("Speed is " + video.playbackRate);
+}
+
+/*skip ahead*/
+document.querySelector("#skip").addEventListener("click", function() {
+	if (video.currentTime += 10 > video.duration) {
+		video.currentTime = 0;
+		console.log("Video Current Time is " + video.currentTime);
+	}
